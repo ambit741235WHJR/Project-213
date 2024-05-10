777ec4e79f608baaefb690d141d27d3656171584
Ambit Biswas <ambit.biswas.whjr@gmail.com>
2024-05-10 18:46:56 +0530

    First Commit

--- remote_Server.py
+++ remote_Server.py
@@ -1,5 +1,59 @@
+# Importing the required libraries
 import socket
 from  threading import Thread
 from pynput.mouse import Button, Controller
 from screeninfo import get_monitors
 from pynput.keyboard import Key, Controller
+
+# Declaring the necessary global variables
+SERVER = None
+PORT = 8000
+IP_ADDRESS = "192.168.0.111"
+screen_width = None
+screen_height = None
+keyboard = Controller()
+
+# Function to get the device size
+def getDeviceSize():
+    global screen_width, screen_height
+    for m in get_monitors():
+        screen_width = int(str(m).split(",")[2].strip().split('width=')[1])
+        screen_height = int(str(m).split(",")[3].strip().split('height=')[1])
+
+# Function to transmit the screen data and the mouse control data from the server side to the client side
+def recvMessage(client_socket):
+    global keyboard
+    while True:
+        try:
+            message = client_socket.recv(2048).decode()
+            if(message):
+                keyboard.press(message)
+                keyboard.release(message)
+                print(message)
+        except Exception as e:
+            pass
+
+# Function to accept connections from the client
+def acceptConnections():
+    global SERVER
+    while True:
+        client_socket, addr = SERVER.accept()
+        print(f"Connection established with {client_socket} : {addr}")
+        thread = Thread(target=recvMessage, args=(client_socket,))
+        thread.start()
+
+# Setup function to initialize the server
+def setup():
+    print("\n\t\t\t\t\t*** Welcome to Remote Mouse ***\n")
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.bind((IP_ADDRESS, PORT))
+    SERVER.listen(10)
+    print("\t\t\t\t\t*** Server is waiting for the client to connect ***\n")
+    getDeviceSize()
+    acceptConnections()
+
+setup()
